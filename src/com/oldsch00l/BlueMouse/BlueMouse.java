4e07a90a59904a99dd2a1fee025b5179d06f4967
Peinthor Rene <peinthor@gmail.com>
2011-03-08 20:27:57 +0100

    initial

---
+++ src/com/oldsch00l/BlueMouse/BlueMouse.java
@@ -0,0 +1,13 @@
+package com.oldsch00l.BlueMouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlueMouse extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
