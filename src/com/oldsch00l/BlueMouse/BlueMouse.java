46b31d29e8416ba00c62fb36898aebcf951150a6
Peinthor Rene <peinthor@gmail.com>
2011-03-23 21:48:56 +0100

    remove the scan menu entry

--- src/com/oldsch00l/BlueMouse/BlueMouse.java
+++ src/com/oldsch00l/BlueMouse/BlueMouse.java
@@ -347,11 +347,6 @@ public class BlueMouse extends MapActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.scan:
-            // Launch the DeviceListActivity to see devices and do scan
-            Intent serverIntent = new Intent(this, DeviceListActivity.class);
-            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
-            return true;
         case R.id.discoverable:
             // Ensure this device is discoverable by others
             ensureDiscoverable();
