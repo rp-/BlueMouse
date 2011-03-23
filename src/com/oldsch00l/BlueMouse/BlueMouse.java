a515f5778122f83839d82fb56f8165c6b98f29b8
Peinthor Rene <peinthor@gmail.com>
2011-03-23 21:47:24 +0100

    add GPLv3

--- src/com/oldsch00l/BlueMouse/BlueMouse.java
+++ src/com/oldsch00l/BlueMouse/BlueMouse.java
@@ -14,6 +14,26 @@
  * limitations under the License.
  */
 
+/*
+ *  This File was edited and further relicensed under GPL v3.
+ *  Copyright (C) 2011 Rene Peinthor.
+ * 
+ *  This file is part of BlueMouse.
+ *
+ *  BlueMouse is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BlueMouse is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.oldsch00l.BlueMouse;
 
 import java.text.DecimalFormat;
