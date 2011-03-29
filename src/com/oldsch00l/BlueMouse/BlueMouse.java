bcb3abca5ae804ba9b3999350def91ce5aac5692
Peinthor Rene <peinthor@gmail.com>
2011-03-29 18:19:35 +0200

    app isn't called foobar

--- src/com/oldsch00l/BlueMouse/BlueMouse.java
+++ src/com/oldsch00l/BlueMouse/BlueMouse.java
@@ -31,7 +31,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with BlueMouse.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.oldsch00l.BlueMouse;
